5c520e0eaa0a7fb42329f7dac6a86bdc6a23d8f2
AtharvaIparkar <143648007+AtharvaIparkar@users.noreply.github.com>
2024-08-08 16:13:53 +0530

    Add files via upload

---
+++ C.G./DDA_AlgoLine.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<graphics.h>
+using namespace std;
+class point {
+public :
+float x0, y0;
+
+void accept() {
+cin>>x0>>y0;
+}
+
+void display() {
+cout<<"point is : ("<<x0<<","<<y0<<")"<<endl;
+}
+
+void DDALine (float x1, float y1, float x2, float y2) {
+float x,y,dx,dy,xinc,yinc,steps;
+dx = x2 - x1;
+dy = y2 - y1;
+x = x1;
+y = y1;
+
+while ((x <= x2) && (y <= y2)) {
+if (dx > dy) {
+steps = dx;
+} else {
+steps = dy;
+}
+xinc = dx/steps;
+yinc = dy/steps;
+x = x + xinc;
+y = y + yinc;
+cout<<"point is : ("<<x<<","<<y<<")"<<endl;
+putpixel(x,y,RED);
+}
+}
+};
+
+int main() {
+point P1, P2, P3;
+
+int gd = DETECT, gm;
+initgraph (&gd, &gm, NULL);
+
+cout<<"Enter the start pt. coordinate : ";
+P1.accept();
+P1.display();
+cout<<"Enter the end pt. coordinate : ";
+P2.accept();
+P2.display();
+
+P3.DDALine(P1.x0, P1.y0, P2.x0, P2.y0);
+delay(50000);
+return 0;
+
+}
