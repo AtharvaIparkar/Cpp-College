7bc36af1dd7a6a53114865f9236a1f3f145d6ad1
AtharvaIparkar <143648007+AtharvaIparkar@users.noreply.github.com>
2024-08-08 14:48:45 +0530

    Add files via upload

---
+++ C.G./Table.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+int n;
+int b;
+cout<<"Enter a no : ";
+cin>>n;
+for (int i=1;i<11;i++) {
+       b = n * i;
+       cout<<b<<endl;     
+    }
+}
